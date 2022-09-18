47f716e39000f51a35a3bb17d94f97f7365cd1c5
Khwanlada <Khwanlada_k@cmu.ac.th>
2022-09-18 20:32:52 +0700

    forget password  template

---
+++ resources/views/admin/template_email/forgotpassword.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    Hey there,
+    Someone requested a new password for your [username] account.
+    
+    This is your new password:[temp_password] .
+    
+     🙂
+
+</body>
+</html>
