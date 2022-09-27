1c13bb15ddc47967bbc6bcba37bc10d6ee870436
Khwanlada <Khwanlada_k@cmu.ac.th>
2022-09-27 20:43:09 +0700

    reateWeights create,edit,index and show in view

---
+++ resources/views/admin/rateWeights/edit.blade.php
@@ -0,0 +1,234 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3 class="page-title">@lang('global.rateWeights.title')</h3>
+    
+    {!! Form::model($rateWeight, ['method' => 'PUT', 'route' => ['admin.rateWeights.update', $rateWeight->id]]) !!}
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+          Edit
+        </div>
+
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="type" class="control-label">น้ำหนักรถ(กิโลกรัม)</label>
+                    {!! Form::text('type', old('type'), ['class' => 'form-control', 'placeholder' => 'Enter type', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('type'))
+                        <p class="help-block">
+                            {{ $errors->first('type') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="start_weight" class="control-label">น้ำหนักเริ่ม</label>
+                    {!! Form::number('start_weight', old('start_weight'), ['class' => 'form-control', 'placeholder' => 'Enter Start Weight', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('start_weight'))
+                        <p class="help-block">
+                            {{ $errors->first('start_weight') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="end_weight" class="control-label">น้ำหนักสิ้นสุด</label>
+                    {!! Form::number('end_weight', old('end_weight'), ['class' => 'form-control', 'placeholder' => 'Enter End Weight', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('end_weight'))
+                        <p class="help-block">
+                            {{ $errors->first('end_weight') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="car1" class="control-label">รถยนต์นั่งส่วนบุคคล เกิน 7 คน</label>
+                    {!! Form::number('car1', old('car1'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('car1'))
+                        <p class="help-block">
+                            {{ $errors->first('car1') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="car2" class="control-label">รถยนต์รับจ้างระหว่างจังหวัด /รถยนต์บริการ</label>
+                    {!! Form::number('car2', old('car2'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('car2'))
+                        <p class="help-block">
+                            {{ $errors->first('car2') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="car3" class="control-label">รถยนต์รับจ้าง</label>
+                    {!! Form::number('car3', old('car3'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('car3'))
+                        <p class="help-block">
+                            {{ $errors->first('car3') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="car4" class="control-label">รถยนต์บรรทุกส่วนบุคคล /รถยนต์ลากจูง /รถแทรกเตอร์ที่มีได้ใช้ในการเกษตร</label>
+                    {!! Form::number('car4', old('car4'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('car4'))
+                        <p class="help-block">
+                            {{ $errors->first('car4') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="legalEntity" class="control-label">นิติบุคคลที่มิได้เป็นผู้ให้เช่าซื้อ(คูณ x)</label>
+                    {!! Form::number('legalEntity', old('legalEntity'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('legalEntity'))
+                        <p class="help-block">
+                            {{ $errors->first('legalEntity') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="ngv_cng" class="control-label">ส่วนลด NGV/CNG(%)</label>
+                    {!! Form::number('ngv_cng', old('ngv_cng'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('ngv_cng'))
+                        <p class="help-block">
+                            {{ $errors->first('ngv_cng') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="hybrid" class="control-label">ส่วนลด Hybrid(%)</label>
+                    {!! Form::number('hybrid', old('hybrid'), ['class' => 'form-control', 'placeholder' => 'Enter number', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('hybrid'))
+                        <p class="help-block">
+                            {{ $errors->first('hybrid') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="percen_late" class="control-label">ค่าปรับเสียภาษีล่าช้า (% เดือน)</label>
+                    {!! Form::number('percen_late', old('percen_late'), ['class' => 'form-control', 'placeholder' => 'Enter Percen Late', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('percen_late'))
+                        <p class="help-block">
+                            {{ $errors->first('percen_late') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="inspection" class="control-label">ค่าตรวจสภาพรถ</label>
+                    {!! Form::number('inspection', old('inspection'), ['class' => 'form-control', 'placeholder' => 'Enter Inspection', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('inspection'))
+                        <p class="help-block">
+                            {{ $errors->first('inspection') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="tax_car_service" class="control-label">ค่าบริการเสียภาษี รถ</label>
+                    {!! Form::number('tax_car_service', old('tax_car_service'), ['class' => 'form-control', 'placeholder' => 'Enter Tax Car Service', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('tax_car_service'))
+                        <p class="help-block">
+                            {{ $errors->first('tax_car_service') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="other_service" class="control-label">อื่นๆ</label>
+                    {!! Form::number('other_service', old('other_service'), ['class' => 'form-control', 'placeholder' => 'Enter Other Service', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('other_service'))
+                        <p class="help-block">
+                            {{ $errors->first('other_service') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="other_service2" class="control-label">อื่นๆ 2</label>
+                    {!! Form::number('other_service2', old('other_service2'), ['class' => 'form-control', 'placeholder' => 'Enter Other Service', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('other_service2'))
+                        <p class="help-block">
+                            {{ $errors->first('other_service2') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="other_service3" class="control-label">อื่นๆ 3</label>
+                    {!! Form::number('other_service3', old('other_service3'), ['class' => 'form-control', 'placeholder' => 'Enter Other Service', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('other_service3'))
+                        <p class="help-block">
+                            {{ $errors->first('other_service3') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    <label for="remark" class="control-label">หมายเหตุ</label>
+                    {!! Form::text('remark', old('remark'), ['class' => 'form-control', 'placeholder' => 'Enter Other Service']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('remark'))
+                        <p class="help-block">
+                            {{ $errors->first('remark') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {!! Form::submit(trans('global.app_update'), ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+@stop
+
+@section('javascript')
+    @parent
+    <script>
+        $('.date').datepicker({
+            autoclose: true,
+            dateFormat: "{{ config('app.date_format_js') }}"
+        });
+    </script>
+
+@stop
---
+++ resources/views/admin/rateWeights/index.blade.php
@@ -0,0 +1,144 @@
+@inject('request', 'Illuminate\Http\Request')
+@extends('layouts.app')
+
+@section('content')
+    <h3 class="page-title">อัตราการเสียภาษีรถ คิดตามน้ำหนักรถ</h3>
+    @can('rateWeight_create')
+    <p>
+        <a href="{{ route('admin.rateWeights.create') }}" class="btn btn-success">@lang('global.app_add_new')</a>
+
+    </p>
+    @endcan
+
+    <p>
+        <ul class="list-inline">
+            <li><a href="{{ route('admin.rateWeights.index') }}" style="{{ request('show_deleted') == 1 ? '' : 'font-weight: 700' }}">@lang('global.app_all')</a></li>
+        @can('rateWeight_delete')
+            | <li><a href="{{ route('admin.rateWeights.index') }}?show_deleted=1" style="{{ request('show_deleted') == 1 ? 'font-weight: 700' : '' }}">@lang('global.app_trash')</a></li>
+        @endcan
+        </ul>
+    </p>
+
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            @lang('global.app_list')
+        </div>
+
+        <div class="panel-body table-responsive">
+            <table class="table table-bordered table-striped {{ count($rateWeights) > 0 ? 'datatable' : '' }} @can('rateWeight_delete') @if ( request('show_deleted') != 1 ) dt-select @endif @endcan">
+                <thead>
+                    <tr>
+                        @can('rateWeight_delete')
+                            @if ( request('show_deleted') != 1 )<th style="text-align:center;"><input type="checkbox" id="select-all" /></th>@endif
+                        @endcan
+
+                        <th>Id</th>
+                        <th>น้ำหนักรถ(กิโลกรัม)</th>
+                        <th>น้ำหนักเริ่ม</th>
+                        <th>น้ำหนักสิ้นสุด</th>
+                        <th>รถยนต์นั่งส่วนบุคคล เกิน 7 คน</th>
+                        <th>รถยนต์รับจ้างระหว่างจังหวัด /รถยนต์บริการ</th>
+                        <th>รถยนต์รับจ้าง</th>
+                        <th>รถยนต์บรรทุกส่วนบุคคล /รถยนต์ลากจูง /รถแทรกเตอร์ที่มีได้ใช้ในการเกษตร</th>
+                        <th>นิติบุคคลที่มิได้เป็นผู้ให้เช่าซื้อ(คูณ x)</th>
+                        <th>ส่วนลด NGV/CNG(%)</th>
+                        <th>ส่วนลด Hybrid(%)</th>
+                            <th>ค่าปรับเสียภาษีล่าช้า (% เดือน)</th>
+                            <th>ค่าตรวจสภาพรถ</th>
+                            <th>ค่าบริการเสียภาษี รถ</th>
+                            <th>อื่นๆ</th>
+                            <th>อื่นๆ 2</th>
+                            <th>อื่นๆ 3</th>
+                            <th>หมายเหตุ</th>
+                        @if( request('show_deleted') == 1 )
+                        <th>&nbsp;</th>
+                        @else
+                        <th>&nbsp;</th>
+                        @endif
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @if (count($rateWeights) > 0)
+                        @foreach ($rateWeights as $rateWeight)
+                            <tr data-entry-id="{{ $rateWeight->id }}">
+                                @can('rateWeight_delete')
+                                    @if ( request('show_deleted') != 1 )<td></td>@endif
+                                @endcan
+
+                                <td field-key='id'>{{ $rateWeight->id }}</td>
+                                <td field-key='type'>{{ $rateWeight->type }}</td>
+                                <td field-key='start_weight'>{{ $rateWeight->start_weight }}</td>
+                                <td field-key='end_weight'>{{ $rateWeight->end_weight }}</td>
+                                <td field-key='car1'>{{ $rateWeight->car1 }}</td>
+                                <td field-key='car2'>{{ $rateWeight->car2 }}</td>
+                                <td field-key='car3'>{{ $rateWeight->car3 }}</td>
+                                <td field-key='car4'>{{ $rateWeight->car4 }}</td>
+                                <td field-key='car4'>{{ $rateWeight->legalEntity }}</td>
+                                <td field-key='car4'>{{ $rateWeight->ngv_cng }}</td>
+                                <td field-key='car4'>{{ $rateWeight->hybrid }}</td>
+                                    <td field-key='percen_late'>{{ $rateWeight->percen_late }}</td>
+                                    <td field-key='inspection'>{{ $rateWeight->inspection }}</td>
+                                    <td field-key='tax_car_service'>{{ $rateWeight->tax_car_service }}</td>
+                                    <td field-key='other_service'>{{ $rateWeight->other_service }}</td>
+                                    <td field-key='other_service2'>{{ $rateWeight->other_service2 }}</td>
+                                    <td field-key='other_service3'>{{ $rateWeight->other_service3 }}</td>
+                                    <td field-key='remark'>{{ $rateWeight->remark }}</td>
+                                @if( request('show_deleted') == 1 )
+                                <td>
+                                    {!! Form::open(array(
+                                        'style' => 'display: inline-block;',
+                                        'method' => 'POST',
+                                        'onsubmit' => "return confirm('".trans("global.app_are_you_sure")."');",
+                                        'route' => ['admin.rateWeights.restore', $rateWeight->id])) !!}
+                                    {!! Form::submit(trans('global.app_restore'), array('class' => 'btn btn-xs btn-success')) !!}
+                                    {!! Form::close() !!}
+                                                                    {!! Form::open(array(
+                                        'style' => 'display: inline-block;',
+                                        'method' => 'DELETE',
+                                        'onsubmit' => "return confirm('".trans("global.app_are_you_sure")."');",
+                                        'route' => ['admin.rateWeights.perma_del', $rateWeight->id])) !!}
+                                    {!! Form::submit(trans('global.app_permadel'), array('class' => 'btn btn-xs btn-danger')) !!}
+                                    {!! Form::close() !!}
+                                                                </td>
+                                @else
+                                <td>
+                                    @can('rateWeight_view')
+                                        <a href="{{ route('admin.rateWeights.show',[$rateWeight->id]) }}" class="btn btn-xs btn-primary">@lang('global.app_view')</a>
+                                    @endcan
+                                    @can('rateWeight_edit')
+                                    <a href="{{ route('admin.rateWeights.edit',[$rateWeight->id]) }}" class="btn btn-xs btn-info">@lang('global.app_edit')</a>
+                                    @endcan
+                                    @can('rateWeight_delete')
+                                    {!! Form::open(array(
+                                        'style' => 'display: inline-block;',
+                                        'method' => 'DELETE',
+                                        'onsubmit' => "return confirm('".trans("global.app_are_you_sure")."');",
+                                        'route' => ['admin.rateWeights.destroy', $rateWeight->id])) !!}
+                                    {!! Form::submit(trans('global.app_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
+                                    {!! Form::close() !!}
+                                    @endcan
+                                </td>
+                                @endif
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="7">@lang('global.app_no_entries_in_table')</td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+        </div>
+    </div>
+@stop
+
+@section('javascript')
+    <script>
+        @can('rateWeight_delete')
+            @if ( request('show_deleted') != 1 ) window.route_mass_crud_entries_destroy = '{{ route('admin.rateWeights.mass_destroy') }}'; @endif
+        @endcan
+
+    </script>
+@endsection
