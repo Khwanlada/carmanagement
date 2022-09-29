803b7e455bb2d64c0d0cde55fff865f691d9455c
Khwanlada <Khwanlada_k@cmu.ac.th>
2022-09-29 13:39:18 +0700

    create carbrand create,edit in views

---
+++ resources/views/admin/carbrands/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3 class="page-title">@lang('global.carbrands.title')</h3>
+    
+    {!! Form::model($carbrand, ['method' => 'PUT', 'route' => ['admin.carbrands.update', $carbrand->id]]) !!}
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            @lang('global.app_edit')
+        </div>
+
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    {!! Form::label('name', trans('global.carbrands.fields.name').'*', ['class' => 'control-label']) !!}
+                    {!! Form::text('name', old('name'), ['class' => 'form-control', 'placeholder' => 'Enter name', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('name'))
+                        <p class="help-block">
+                            {{ $errors->first('name') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    {!! Form::submit(trans('global.app_update'), ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+@stop
+
+@section('javascript')
+    @parent
+    <script>
+        $('.date').datepicker({
+            autoclose: true,
+            dateFormat: "{{ config('app.date_format_js') }}"
+        });
+    </script>
+
+@stop
