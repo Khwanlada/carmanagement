7bc452237dc3f9f620231319a2922f07e232aaa0
Khwanlada <Khwanlada_k@cmu.ac.th>
2022-09-29 12:11:58 +0700

    create reateWeights model

---
+++ app/RateWeight.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RateWeight extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['type', 'start_weight', 'end_weight', 'car1', 'car2', 'car3', 'car4', 'legalEntity', 'ngv_cng', 'hybrid', 'percen_late', 'inspection', 'tax_car_service', 'other_service', 'other_service2', 'other_service3', 'remark'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+}
